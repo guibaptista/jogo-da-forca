0562a94447c9e9a453ef86fe81a5fee51275969b
Guilherme Baptista Pontes Alves <87674771+guibaptista@users.noreply.github.com>
2021-08-12 15:29:52 -0300

    Update jogo_forca.py

--- jogo_forca.py
+++ jogo_forca.py
@@ -71,12 +71,12 @@ class Hangman:
 				
 	# Método para adivinhar a letra
 	def guess(self, letter):
-	    if letter in self.word and letter not in self.guessed letters:
-		self.guessed_letters.append(letter)
-	    elif letter not in self.word and letter not in self.mssed.letters:
-		self.missed_letters.append(letter)
+	    if letter in self.word and letter not in self.guessed_letters:
+		      self.guessed_letters.append(letter)
+	    elif letter not in self.word and letter not in self.missed_letters:
+		       self.missed_letters.append(letter)
 	    else:
-		return False
+		      return False
 	    return True
 		
 	# Método para verificar se o jogo terminou
@@ -86,17 +86,17 @@ class Hangman:
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
 	    if '_' not in self.hide_word():
-		return True
+	      	return True
 	    return False		
 
 	# Método para não mostrar a letra no board
 	def hide_word(self):
 	    rtn = ''
 	    for letter in self.word:
-		if letter not in self.guessed_letters:
-		    rtn += '_'
-		else:
-		    rtn += letter
+		    if letter not in self.guessed_letters:
+		        rtn += '_'
+		    else:
+		      rtn += letter
 	    return rtn
 		
 	# Método para checar o status do game e imprimir o board na tela
@@ -109,12 +109,12 @@ class Hangman:
 	    print()	
 	    print ('Letras corretas: ',)
 	    for letter in self.guessed_letters:
-		print (letter,)
+		      print (letter,)
 	    print()
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
-        with open("palavras.txt", "rt") as f:
+        with open("/content/palavras.txt", "rt") as f:
                 bank = f.readlines()
         return bank[random.randint(0,len(bank))].strip()
 
